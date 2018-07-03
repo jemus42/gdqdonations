38d3610641e43ebdfccb8a9bef1a246b041d4cb0
jemus42 <lukas@quantenbrot.de>
2018-07-03 23:10:12 +0200

    Add build notification

--- _build.R
+++ _build.R
@@ -1,3 +1,9 @@
 #! /usr/bin/env Rscript
 
 rmarkdown::render_site(".")
+
+library(slackr)
+slackr_setup(config_file = "/opt/tadaadata/.slackr")
+
+msg <- paste0(lubridate::now(), ": Built https://gdq.tadaa-data.de")
+text_slackr(msg, channel = "#gdq", username = "tadaabot", preformatted = FALSE)
