896aeb04ebe12b4e78518b87878395ddf96b6c7a
Lukas Burk <lukas@quantenbrot.de>
2018-07-11 15:34:00 +0200

    Fix tz

--- _build.R
+++ _build.R
@@ -5,5 +5,5 @@ rmarkdown::render_site(".")
 library(slackr)
 slackr_setup(config_file = "/opt/tadaadata/.slackr")
 
-msg <- paste0(lubridate::now(), ": Built https://gdq.tadaa-data.de")
+msg <- paste0(lubridate::now(tzone = "CET"), ": Built https://gdq.tadaa-data.de")
 text_slackr(msg, channel = "#gdq", username = "tadaabot", preformatted = FALSE)
